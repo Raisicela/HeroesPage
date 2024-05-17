d6e16c8acdcd6aa504d34ad35aff5e4574da98e8
Raisa Guayasamin <raisa.marilyn1993@gmail.com>
2024-05-17 10:06:53 -0500

    Proteccion de rutas con Can MatchFn y Can ActiveFn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+import { authActivateGuard } from './auth/guards/auth-activate.guard';
+import { authMatchGuard } from './auth/guards/auth-match.guard';
+import { publicActivateGuard } from './auth/guards/public-activate.guard';
+import { publicMatchGuard } from './auth/guards/public-match.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [publicActivateGuard],
+    canMatch: [publicMatchGuard],
   },
   {
     path: 'heroes',
     loadChildren: () =>
       import('./heroes/heroes.module').then((m) => m.HeroesModule),
+    canActivate: [authActivateGuard],
+    canMatch: [authMatchGuard],
   },
   {
     path: '404',
